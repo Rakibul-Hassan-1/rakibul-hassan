8fb13c64bb562a6acd43ef29b520bf86ef3f3809
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-10-25 20:57:36 +0600

    new

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)6lqc1*+_2(%(9$g*j#g045@a4&a76+b!9ns1gsy$aap#p3b*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 

127e4fdc242348a98fc41d6c885fb664f39749a5
rakibul-hassan-1 <rakibulhassan29052000@gmail.com>
2024-10-25 22:54:03 +0600

    site acc

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -124,4 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_COOKIE_SECURE = False
+CSRF_TRUSTED_ORIGINS = ['https://cn2en.onrender.com']
+
+CSRF_COOKIE_SECURE = True

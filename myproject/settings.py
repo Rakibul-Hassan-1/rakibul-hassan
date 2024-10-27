26eb2b2e8519d1acc12ba07294e0956615c2edd5
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-10-27 23:00:26 +0600

    fixed

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-)6lqc1*+_2(%(9$g*j#g045@a4&a76+b!9ns1gsy$aap#p3b*n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
@@ -79,10 +80,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'BwOsuhKWdgixoimjypWDSzvTSNbgqRLQ',
+        'HOST': 'postgres.railway.internal',
+        'PORT': '5432',
     }
 }
 
@@ -130,3 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ['https://cn2en.onrender.com']
 
 CSRF_COOKIE_SECURE = True
+
+STATIC_URL = 'static/'
+import os
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,8 +1,13 @@
 from django.urls import include, path
 from subtitles.views import file_upload
 from subtitles.views import upload_file
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('subtitles.urls')), 
     path('speech_app/', include('speech_app.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

d65a85b9b6f30acf96ff8893dea53955568346f3
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-10-27 23:07:45 +0600

    last change

--- myproject/settings.py
+++ myproject/settings.py
@@ -143,8 +143,10 @@ CSRF_TRUSTED_ORIGINS = ['https://cn2en.onrender.com']
 
 CSRF_COOKIE_SECURE = True
 
-STATIC_URL = 'static/'
 import os
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'

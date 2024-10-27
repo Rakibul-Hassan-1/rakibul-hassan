010473005775b13d0314442017c2d3d55d28a1c0
Rakibul-Hassan-1 <rakibulhassan29052000@gmail.com>
2024-10-27 22:23:01 +0600

    ready for vercel deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)6lqc1*+_2(%(9$g*j#g045@a4&a76+b!9ns1gsy$aap#p3b*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "vercel.app"]
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
